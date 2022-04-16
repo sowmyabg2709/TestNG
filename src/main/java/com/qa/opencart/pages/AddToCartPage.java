c98079a5dc75b410f49d2233311cb038ac1bbafd
sowmyabg2709 <sowmya.bg27@gmail.com>
2022-04-17 01:20:13 +0530

    added locators and pageaction

--- src/main/java/com/qa/opencart/pages/AddToCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddToCartPage.java
@@ -1,5 +1,6 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AddToCartPage {
@@ -7,5 +8,10 @@ public class AddToCartPage {
 	WebDriver driver;
 	public AddToCartPage(WebDriver driver) {
 		this.driver= driver;
+	}
+	
+	private By cart = By.id("cart");
+	public static void addedCart() {
+		System.out.println("added to cart");
 	}
 }
