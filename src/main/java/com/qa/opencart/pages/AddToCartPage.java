866c4f8e929a603d8744680e5f8d3c42090cc98d
sowmyabg2709 <sowmya.bg27@gmail.com>
2022-04-17 01:03:57 +0530

    page is added

---
+++ src/main/java/com/qa/opencart/pages/AddToCartPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AddToCartPage {
+
+	WebDriver driver;
+	public AddToCartPage(WebDriver driver) {
+		this.driver= driver;
+	}
+}
